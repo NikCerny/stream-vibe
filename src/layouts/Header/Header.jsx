f0a353ead27daa3890b2b4e9a50b3835b60bf474
Nick <nickcherny2@gmail.com>
2026-01-18 05:36:17 +0100

    add logo component

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,14 @@
+import './Header.scss'
+import Logo from '@/components/Logo'
+
 const Header = () => {
-    return <header>HEAD</header>
+    return (
+    <header className='header'>
+        <div className="header__inner container">
+            <Logo loading='eager' className='header__logo'></Logo>
+        </div>
+    </header>
+    )
 }
 
 export default Header
