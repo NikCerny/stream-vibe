4de560000fd6bea51dbaee4cc1d55372e96428d9
Nick <nickcherny2@gmail.com>
2026-02-11 18:23:34 +0100

    add section component

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,37 +1,21 @@
 import "./Categories.scss"
+import Section from "@/layouts/Section"
 
 const Categories = () => {
   return (
-    <section className="hero" aria-labelledby="hero-title">
-      <div className="hero__pano">
-        <div className="hero__pano-inner">
-          <PlayButton></PlayButton>
+    <Section
+      title="Explore our wide variety of categories"
+      titleId="categories-title"
+      description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
+      actions={
+        <div>
+          <button>Back</button>
         </div>
-      </div>
-      <div className="hero__body">
-        <div className="hero__body-inner container">
-          <h1 className="hero__title" id="hero-title">
-            The Best Streaming Experience
-          </h1>
-          <div className="hero__description">
-            <p>
-              StreamVibe is the best streaming experience for watching your
-              favorite movies and shows on demand, anytime, anywhere. With
-              StreamVibe, you can enjoy a wide variety of content, including the
-              latest blockbusters, classic movies, popular TV shows, and more.
-              You can also create your own watchlists, so you can easily find
-              the content you want to watch.
-            </p>
-          </div>
-          <Button
-            className="hero__button"
-            iconName="play"
-            hasFillIcon
-            label="Start Watching Now"
-          ></Button>
-        </div>
-      </div>
-    </section>
+      }
+      isActionsHiddenOnMobile
+    >
+      TEXT EXAMPLE
+    </Section>
   )
 }
 
